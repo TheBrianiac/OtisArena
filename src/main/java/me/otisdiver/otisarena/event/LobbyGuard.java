0453d3e608a069f71f7a8355aecff3eab85b8f62
Brian Schultz <bschultz731@gmail.com>
2017-01-23 22:35:24 -0500

    update - prevent players from editing kit selection menu

--- src/main/java/me/otisdiver/otisarena/event/LobbyGuard.java
+++ src/main/java/me/otisdiver/otisarena/event/LobbyGuard.java
@@ -6,6 +6,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 
 import me.otisdiver.otisarena.OtisArena;
 
@@ -32,6 +34,16 @@ public class LobbyGuard extends EasyListener {
     @EventHandler(priority = EventPriority.LOW)
     public void onDamage(EntityDamageEvent e) {
         if (inLobby(e.getEntity())) cancel(e);
+    }
+    
+    @EventHandler(priority = EventPriority.LOW)
+    public void onDrop(PlayerDropItemEvent e) {
+        if (inLobby(e.getPlayer())) cancel(e);
+    }
+    
+    @EventHandler(priority = EventPriority.LOW)
+    public void onInvEdit(InventoryClickEvent e) {
+        if (inLobby(e.getWhoClicked())) cancel(e);
     }
     
     // Super lazy shortcut methods
