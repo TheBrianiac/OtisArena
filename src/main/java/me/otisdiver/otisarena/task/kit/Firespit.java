6b97c59252250b2217a07f2182cb11a0020862f4
Brian Schultz <bschultz731@gmail.com>
2017-01-25 18:07:06 -0500

    create Firespit ability

---
+++ src/main/java/me/otisdiver/otisarena/task/kit/Firespit.java
@@ -0,0 +1,25 @@
+package me.otisdiver.otisarena.task.kit;
+
+import org.bukkit.entity.Fireball;
+import org.bukkit.entity.Player;
+
+import me.otisdiver.otisarena.OtisArena;
+
+public class Firespit extends Ability {
+    
+    private final long waitMillis = 3000;
+    
+    public Firespit(OtisArena main, Player player) {
+        super(main, player);
+    }
+
+    @Override
+    public void run() {
+        if (!super.registerUse(this.getClass(), waitMillis)) return;
+        
+        // shoot a fireball out of the player
+        Fireball fireball = player.launchProjectile(Fireball.class);
+        fireball.setVelocity(player.getLocation().getDirection().multiply(10));
+    }
+    
+}
