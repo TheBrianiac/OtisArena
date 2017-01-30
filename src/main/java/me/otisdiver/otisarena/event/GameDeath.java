95d0111bd1ec20c4552a211c37a07501c29aedaf
Brian Schultz <bschultz731@gmail.com>
2017-01-29 21:35:14 -0500

    update - track arrow kills needs improvement

--- src/main/java/me/otisdiver/otisarena/event/GameDeath.java
+++ src/main/java/me/otisdiver/otisarena/event/GameDeath.java
@@ -3,11 +3,13 @@ package me.otisdiver.otisarena.event;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
+import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.projectiles.ProjectileSource;
 
 import me.otisdiver.otisarena.OtisArena;
 import me.otisdiver.otisarena.game.GameState;
@@ -28,8 +30,8 @@ public class GameDeath extends EasyListener {
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onDamage(EntityDamageEvent e) {
         // conditions (we only care about players during the game)
-        if (!(e.getEntity() instanceof Player)) return;
         if (!GameState.getCurrent().equals(GameState.PLAYING)) return;
+        if (!(e.getEntity() instanceof Player)) return;
         
         Player victim = (Player) e.getEntity();
         
@@ -45,13 +47,30 @@ public class GameDeath extends EasyListener {
     
     @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
     public void onAttack(EntityDamageByEntityEvent e) {
-        // conditions (we only care about players during the game)
-        if (!(e.getDamager() instanceof Player) || !(e.getEntity() instanceof Player)) return;
+        // if a game's not underway, stop
         if (!GameState.getCurrent().equals(GameState.PLAYING)) return;
         
-        // find the attacker and victim
-        Player attacker = (Player) e.getDamager();
-        Player victim = (Player) e.getEntity();
+        Player victim;
+        Player attacker;
+        
+        // find the victim
+        if (e.getEntity() instanceof Player) {
+            victim = (Player) e.getEntity();
+        }
+        // victim not a player, stop
+        else return;
+        
+        // find the attacker
+        if (e.getDamager() instanceof Player) {
+            attacker = (Player) e.getDamager();
+        }
+        else if (e.getDamager() instanceof Arrow) {
+            ProjectileSource shooter = ((Arrow) e.getDamager()).getShooter();
+            if (!(shooter instanceof Player)) return;
+            attacker = (Player) shooter;
+        }
+        // attacker not a player, stop
+        else return;
         
         // if the attack wasn't fatal, stop
         double health = victim.getHealth() - e.getFinalDamage();
