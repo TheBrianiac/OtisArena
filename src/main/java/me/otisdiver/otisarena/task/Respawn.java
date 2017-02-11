69eef586e00b9e23d56a0321ead5107353fab670
Brian Schultz <bschultz731@gmail.com>
2017-02-11 17:47:50 -0500

    fix respawns after game ends

--- src/main/java/me/otisdiver/otisarena/task/Respawn.java
+++ src/main/java/me/otisdiver/otisarena/task/Respawn.java
@@ -5,22 +5,25 @@ import org.bukkit.entity.Player;
 
 import me.otisdiver.otisarena.OtisArena;
 import me.otisdiver.otisarena.game.Game;
+import me.otisdiver.otisarena.game.GameState;
 import me.otisdiver.otisarena.game.InventoryBuilder;
 import me.otisdiver.otisarena.utils.ConfigUtils;
 
 public class Respawn extends Task {
     
     private final Player player;
-    private final Game game;
+    private final Game   game;
     
     public Respawn(OtisArena main, Player player) {
         super(main);
         this.game = main.getGame();
         this.player = player;
     }
-
+    
     @Override
     public void run() {
+        // if a game's not underway, stop
+        if (!GameState.getCurrent().equals(GameState.PLAYING)) return;
         
         player.setGameMode(GameMode.SURVIVAL);
         player.teleport(ConfigUtils.getRandomSpawn(game.getActiveWorld()));
